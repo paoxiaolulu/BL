6a4004aa4613c9d951d1c711f0389da8f597db01
paoxiaolulu <471535626@qq.com>
2018-11-13 17:11:15 +0800

    v5更新

---
+++ testcases/debugtalk.py
@@ -0,0 +1,27 @@
+def get_category_id():
+    return [
+        {"category_id": 1},
+        {"category_id": ''},
+        {"category_id": 'h'},
+        {"category_id": '~'}]
+
+def get_type():
+    return [
+        {"type": 1},
+        {"type": 2},
+        {"type": 3},
+        {"type": '~'}]
+
+def get_page():
+    return [
+        {"page": 1},
+        {"page": ''},
+        {"page": 'h'},
+        {"page": '~'}]
+
+def get_limit():
+    return [
+        {"limit": 1},
+        {"limit": ''},
+        {"limit": 'h'},
+        {"limit": '~'}]
