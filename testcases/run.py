64f0509249bf76cb4c5631e508e9431364e41b0b
paoxiaolulu <471535626@qq.com>
2018-11-13 19:06:56 +0800

    v6更新

--- testcases/run.py
+++ testcases/run.py
@@ -1,8 +1,9 @@
 from httprunner import HttpRunner
 
 runner = HttpRunner()
-runner.run('/Users/air/Documents/BL/testcases')   # 运行测试集合
+runner.run('/Users/air/Documents/BL/testcases/39_礼品库_我的礼品库.yaml')   # 运行单个测试
+# runner.run('/Users/air/Documents/BL/testcases')  # 运行测试集合
 summary = runner.summary
 runner.gen_html_report(
-    html_report_name="v6接口测试",
-    html_report_template="/Users/air/Documents/BL/reports")
+    html_report_name="v6接口测试"
+)
